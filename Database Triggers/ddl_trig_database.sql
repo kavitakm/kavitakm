2a0e70392262f7d0ac22c3b36e76b73f07e44306
Guru Kiran <guru@tesserinsights.com>
2020-12-14 14:07:10 +0530

    DB code changes for bug fixes release_12142020

--- Database Triggers/ddl_trig_database.sql
+++ Database Triggers/ddl_trig_database.sql
@@ -5,6 +5,7 @@ AS
 BEGIN
 /*
 3-DEC-2020	Srimathi	Added calling program name check to remove duplicates, and allow sandbox entries
+14-DEC-2020	Srimathi	Added LoadType with value 1 to insert statement
 */
 DECLARE @id int;
 DECLARE @obj_ID int;
@@ -20,7 +21,7 @@ declare @prog varchar(100);
 	SET @sessionid = EVENTDATA().value('(/EVENT_INSTANCE/SPID)[1]','int')
 	
 	select @prog = [PROGRAM_NAME] from sys.dm_exec_sessions where session_id = @sessionid
-		
+	
 	SELECT @id  = USERID FROM APPADMIN.TI_ADM_USER_LU WHERE USEREMAIL=trim(EVENTDATA().value('(/EVENT_INSTANCE/LoginName)[1]','varchar(max)'))
 	--insert into sandbox.test values(1,EVENTDATA().value('(/EVENT_INSTANCE/LoginName)[1]','varchar(max)'));
     --PRINT 'TEST'
@@ -28,7 +29,7 @@ declare @prog varchar(100);
 	BEGIN
 		--SET @create_trig = 'CREATE TRIGGER trg_' +  @schmname + '_' + @tblname + ' ON ' + @schmname +'.'+ @tblname + ' FOR INSERT, UPDATE, DELETE AS BEGIN ' + ' EXEC [AppAdmin].[ti_analyze_AutoUnivariate_sp] ''' + @schmname + ''', ''' + @tblname + ''', ''' + EVENTDATA().value('(/EVENT_INSTANCE/LoginName)[1]','varchar(max)') + ''' END'
 		--EXECUTE sp_executesql @create_trig    
-		INSERT INTO [AppAdmin].[ti_adm_ObjectOwner] (ObjectName, ObjectType, SchemaName, CreatedDate, LastUpdatedDate ,	IsActive , CreatedBy , LastUpdatedBy, ObjectLocation, FileExt, FileSize) values (EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]','nvarchar(max)') , 'Table', EVENTDATA().value('(/EVENT_INSTANCE/SchemaName)[1]','nvarchar(max)'), EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'),  EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'), 1, @id , @id,'','',0);       
+		INSERT INTO [AppAdmin].[ti_adm_ObjectOwner] (ObjectName, ObjectType, SchemaName, CreatedDate, LastUpdatedDate ,	IsActive , CreatedBy , LastUpdatedBy, ObjectLocation, FileExt, FileSize, LoadType) values (EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]','nvarchar(max)') , 'Table', EVENTDATA().value('(/EVENT_INSTANCE/SchemaName)[1]','nvarchar(max)'), EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'),  EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'), 1, @id , @id,'','',0,1);       
 		SET @obj_ID = SCOPE_IDENTITY();
 		INSERT INTO APPADMIN.ti_adm_ObjectAccessGrant(OBJECTID, GrantToUser, CREATEDDATE, CREATEDBY, LastUpdatedDate, LastUpdatedBy, ISACTIVE, FAVOURITE) 
 			SELECT @obj_ID, USERID, EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'), @id, EVENTDATA().value('(/EVENT_INSTANCE/PostTime)[1]','datetime'), @id, 1, 0
@@ -37,8 +38,6 @@ declare @prog varchar(100);
 	
 	END
 END	
-GO
-DISABLE TRIGGER [ddl_trig_database] ON DATABASE
 GO
 ENABLE TRIGGER [ddl_trig_database] ON DATABASE
 GO
