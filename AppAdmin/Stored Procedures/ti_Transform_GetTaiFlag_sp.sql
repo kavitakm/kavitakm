dcd75a418dfc1f88fa565e2752d6be85a2d978cc
dineshATtesserinsights <75125814+dineshATtesserinsights@users.noreply.github.com>
2021-03-29 12:20:31 +0530

    GetTaiFlag

--- AppAdmin/Stored Procedures/ti_Transform_GetTaiFlag_sp.sql
+++ AppAdmin/Stored Procedures/ti_Transform_GetTaiFlag_sp.sql
@@ -1,20 +1,17 @@
-﻿-- exec  [AppAdmin].[ti_Transform_GetTaiFlag_sp]  '10010'
-
-CREATE PROC [AppAdmin].[ti_Transform_GetTaiFlag_sp]        
-@TransformID int        
-as         
-BEGIN  
-/**************************************************************************                      
-** Version               : 1.0                             
-** Author                : Harini                      
-** Description           : Get the TAI_Enabled flag from ti_adm_ObjectOwner table                   
-** Date					 : 29-March-2021                      
-                       
-*******************************************************************************/                            
-	SELECT   TAI_Enabled  
-	FROM [AppAdmin].[ti_adm_ObjectOwner] obj               
-	WHERE obj.ObjectId = @TransformID      
-END  
+﻿CREATE PROC [AppAdmin].[ti_Transform_GetTaiFlag_sp]          
+@TransformID int          
+as           
+BEGIN    
+/**************************************************************************                        
+** Version               : 1.0                               
+** Author                : Harini                        
+** Description           : Get the TAI_Enabled flag from ti_adm_ObjectOwner table                     
+** Date      : 29-March-2021                        
+                         
+*******************************************************************************/                              
+ SELECT   obj.TAI_Enabled    
+ FROM [AppAdmin].[ti_adm_ObjectOwner] obj  
+ INNER JOIN [AppAdmin].[ti_adm_transform] T on obj.Objectid = T.TargetObjectID                        
+ WHERE T.ObjectId = @TransformID        
+END 
 GO
-
-
