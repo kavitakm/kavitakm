c071d8ace50a0022d89adc1504f56d143e240ea3
dineshATtesserinsights <75125814+dineshATtesserinsights@users.noreply.github.com>
2021-04-15 10:11:15 +0530

    Changes on 15apr21

--- AppAdmin/Stored Procedures/ti_Transform_GetTaiFlag_sp.sql
+++ AppAdmin/Stored Procedures/ti_Transform_GetTaiFlag_sp.sql
@@ -1,17 +1,26 @@
-﻿CREATE PROC [AppAdmin].[ti_Transform_GetTaiFlag_sp]          
-@TransformID int          
-as           
-BEGIN    
-/**************************************************************************                        
-** Version               : 1.0                               
-** Author                : Harini                        
-** Description           : Get the TAI_Enabled flag from ti_adm_ObjectOwner table                     
-** Date      : 29-March-2021                        
-                         
-*******************************************************************************/                              
- SELECT   obj.TAI_Enabled    
- FROM [AppAdmin].[ti_adm_ObjectOwner] obj  
- INNER JOIN [AppAdmin].[ti_adm_transform] T on obj.Objectid = T.TargetObjectID                        
- WHERE T.ObjectId = @TransformID        
+﻿CREATE PROC [AppAdmin].[ti_Transform_GetTaiFlag_sp]              
+@ObjectID int     
+, @ObjectType VARCHAR (20) = 'Transform'           
+as               
+BEGIN        
+/**************************************************************************                            
+** Version               : 1.0                                   
+** Author                : Harini                            
+** Description           : Get the TAI_Enabled flag from ti_adm_ObjectOwner table                         
+** Date      : 29-March-2021                            
+                             
+*******************************************************************************/      
+IF (@ObjectType = 'Transform')  
+ BEGIN  
+  SELECT   obj.TAI_Enabled        
+  FROM [AppAdmin].[ti_adm_ObjectOwner] obj      
+  INNER JOIN [AppAdmin].[ti_adm_transform] T on obj.Objectid = T.TargetObjectID                            
+  WHERE T.ObjectId = @ObjectID    
+ END  
+ELSE  
+ BEGIN                              
+  SELECT   obj.TAI_Enabled        
+  FROM [AppAdmin].[ti_adm_ObjectOwner] obj   
+  WHERE obj.Objectid = @ObjectID  
+ END           
 END 
-GO
